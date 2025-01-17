2d57e408d4ca658cc99037fec3738eb6117cc9c4
Amir <itzdele1372@gmail.com>
2025-01-17 07:15:29 +0330

    db test

--- config/products/urls.py
+++ config/products/urls.py
@@ -3,14 +3,8 @@ from products import views
 
 urlpatterns = [
     path("advertisements/", views.AdvertisementView.as_view(), name="advertisements"),
-    path(
-        "alladvertisements/",
-        views.AllAdvertisementsView.as_view(),
-        name="alladvertisements",
-    ),
-    path(
-        "advertisementdetail/",
-        views.AdvertisementDetailView.as_view(),
-        name="advertisementdetail",
-    ),
+
+    path( "alladvertisements/",views.AllAdvertisementsView.as_view(),name="alladvertisements",),
+
+    path("advertisementdetail/",views.AdvertisementDetailView.as_view(),name="advertisementdetail",),
 ]
