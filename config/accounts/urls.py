6cf1c714f7258b36bb5f090877a86af00fac2019
Amir <itzdele1372@gmail.com>
2025-01-17 04:05:01 +0330

    fixed view.py

--- config/accounts/urls.py
+++ config/accounts/urls.py
@@ -1,10 +1,10 @@
 # urls.py
 from django.urls import path
-from .views import RegisterView, LoginView ,send_otp ,verify_otp
+from accounts import views
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("send_otp/", send_otp(), name="send_otp"),
-    path("verify_otp/", verify_otp(), name="verify_otp"),
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("send_otp/", views.send_otp, name="send_otp"),
+    path("verify_otp/", views.verify_otp, name="verify_otp"),
 ]
